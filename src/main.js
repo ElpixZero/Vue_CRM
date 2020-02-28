2d6b25484b1314e27f68011cb99a6677d8708690
ElpixJunior <nasurov.eldar@mail.ru>
2020-02-29 01:38:38 +0300

    Added filter date transformation func

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router.js";
 import store from "./store";
+import dateFilter from "./filters/date.filter";
 import "materialize-css/dist/js/materialize.min";
+import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+Vue.filter("date", dateFilter);
 
 new Vue({
   router,
