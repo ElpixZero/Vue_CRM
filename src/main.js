f8cce1c2f68d79ce8761ff8326f6f1ad4e3f41f5
ElpixJunior <nasurov.eldar@mail.ru>
2020-02-29 12:56:14 +0300

    Added validate package

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import Vue from "vue";
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router.js";
 import store from "./store";
 import dateFilter from "./filters/date.filter";
-import "materialize-css/dist/js/materialize.min";
 import "./registerServiceWorker";
+import "materialize-css/dist/js/materialize.min";
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuelidate);
 Vue.filter("date", dateFilter);
 
 new Vue({
